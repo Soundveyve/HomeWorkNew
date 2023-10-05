f41a14aaadfd09c7ceb93af002d1cc7659b9bd7e
Soundveyve <coksharov.kirill2015@yandex.ru>
2023-10-05 23:49:16 +0300

    Homework by 06.10

--- SetsTasks/Task2.py
+++ SetsTasks/Task2.py
@@ -6,4 +6,15 @@
 """
 testList = [1,2,2,[3,4],(1,2,3),"1",{1,2,3}]
 printSet = set()
-
+list_edit = []
+for i in testList:
+    if type(i) == set or type(i) == list or type(i) == dict:
+        list_edit.append(i)
+        print (i)
+        print(False)
+    else:
+        print (i)
+        print(True)
+        printSet.add(i)
+print (f"Изменяемые элементы, которые не были включены во множество: {list_edit}")
+print (f"Множество из изменяемых элементов: {printSet}")
