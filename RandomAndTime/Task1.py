92bcf77310a37e14cce66879c0a392516f94d997
Soundveyve <coksharov.kirill2015@yandex.ru>
2023-10-13 11:55:24 +0300

    A little work on Function and Random/Time

--- RandomAndTime/Task1.py
+++ RandomAndTime/Task1.py
@@ -5,3 +5,23 @@
 Если 30 минут закончились или игрок вводит «off» — завершать работу.
 Оформить в виде функции.
 """
+import time
+def give_me_time ():
+    start = time.time()
+    move = input("Введите ваш ход: ")
+    if move != "off" and start != start/60:
+        ready = time.time()
+        final_time = ready - start
+        print (f"Оставшееся время: {30 - final_time/60:.2f} минут")
+def give_me_time2 ():
+    game_time = 1800
+    while game_time > 0:
+        start = time.time()
+        move = input("Введите ваш ход: ")
+        end = time
+        game_time -= end - start
+        if move == "off":
+            break
+        print(f"У вас осталось: {game_time}")
+    print("ИГРА ОКОНЧЕНА!")
+give_me_time()
