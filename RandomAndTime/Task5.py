d6d484be920db2d5f55d8b7efd179321edf26b4c
Soundveyve <coksharov.kirill2015@yandex.ru>
2023-10-21 15:01:12 +0300

    Generic

--- RandomAndTime/Task5.py
+++ RandomAndTime/Task5.py
@@ -8,14 +8,14 @@ from random import randint
 from time import sleep
 def cobe ():
     responce = "да"
-    yes_list = ["yes","да"]
+    yes_list = ["yes", "да"]
     while responce in yes_list:
         toss1 = random.randint(1, 6)
         toss2 = random.randint(1, 6)
         print("ПОДБРАСЫВАЮ КУБИКИ!")
         time.sleep(2)
         print(toss1, toss2)
-        print ("Нужно ли вновь бросить кубики?")
+        print("Нужно ли вновь бросить кубики?")
         responce = input("Ваш ответ: ").lower()
 def main ():
     cobe()
